209cd96fb64189b6d7ab2776e39f5f71e60f03a3
George Oliveira Barros <geogobgob@gmail.com>
2014-09-03 15:51:40 -0300

    first work with interfaces
    Adcionando a pasta como primeiro trabalho com interfaces ao diretório
    básico

--- basic/funcAndModulos.py
+++ basic/funcAndModulos.py
@@ -1,1 +1,22 @@
-#Pag 26 - Funções e Módulos
+#Funções
+
+
+x = int(raw_input('Informe um valor'))
+
+
+def soma(x,y):
+    x = x+y
+    return x
+
+def somatorio (*argumentos):
+    result = 0
+    for i in argumentos:
+        result+=i
+    return result
+
+
+
+print 'valor + 5 = '+ str(soma(x,5))
+
+print 'somatorio = '+ str(somatorio(5,12,22))
+
---
+++ interface/trab1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 03 14:29:03 2014
+
+@author: George Oliveira Barros. 
+Atividade da disciplina de Fundamentos de Processamento de Imagens.
+Programa de Pós Graduação em Computação Aplicada - Mestrado. UEFS. Bahia.
+"""
+
+from Tkinter import *
+class Janela:
+    def __init__(self,toplevel):
+        #frames
+        self.fr1 = Frame(toplevel)
+        self.fr1.pack()
+        
+        self.fr2 = Frame(toplevel)
+        self.fr2.pack()
+        
+        #Bottons
+        self.botao = Button(self.fr2, text='button 1')
+        self.botao['bg']='white'
+        self.botao['font']=('Arial','12')
+        self.botao['height']=10
+        self.botao['width']=10
+        self.botao.pack(side=LEFT)
+        
+        self.botao2 = Button(self.fr2, text='button 2')
+        self.botao2['bg']='gray'
+        self.botao2['font']=('Arial','12')
+        self.botao2['height']=5
+        self.botao2['width']=10
+        self.botao2.pack()
+        
+        
+raiz=Tk()
+Janela(raiz)
+raiz.mainloop()
